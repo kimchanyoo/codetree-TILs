b7f6234fe0c656653ec24c07008904fed34cadcb
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-03 16:28:23 +0900

    Title: 씨 오 더블유 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240203/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String str = sc.next();
+		String[] strList = str.split("");
+		int cnt = 0;
+
+		for(int i = 0; i < n - 2; i++){
+			if(strList[i].equals("C")){
+				for(int j = i + 1; j < n - 1; j++){
+					if(strList[j].equals("O")){
+						for(int k = i + 1; k < n; k++){
+							if(strList[k].equals("W")){
+								cnt++;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+    }
+}
