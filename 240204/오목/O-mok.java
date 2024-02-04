b09758d7c205016c4de6a3adce79f299b4f243ba
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-04 20:24:42 +0900

    Title: 오목, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240204/오목/O-mok.java
+++ 240204/오목/O-mok.java
@@ -1,48 +1,52 @@
 import java.util.Scanner;
 public class Main {
-    static int[] dx = {-1, 0, 0, 1}, dy = {0, -1, 1, 0};
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-		int[][] area = new int[19][19];
-		for(int i = 0; i < 19; i++){
-			for(int j = 0; j < 19; j++){
-				area[i][j] = sc.nextInt();
-			}
-		}
-		for(int i = 0; i < 19; i++) {
-			for(int j = 0; j < 19; j++) {
-				if(area[i][j] == 0) continue;
+    public static final int DIR_NUM = 8;
 
-				for(int k = 0; k < 4; k++) { // 모든 방향 탐색
+	public static int[][] arr = new int[19][19];
+
+	public static int[] dx = new int[]{1, 1, 1, -1, -1, -1, 0, 0};
+	public static int[] dy = new int[]{-1, 0, 1, -1, 0, 1, -1, 1};
+
+	public static boolean inRange(int x, int y) {
+		return 0 <= x && x < 19 && 0 <= y && y < 19;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 입력
+		for(int i = 0; i < 19; i++)
+			for(int j = 0; j < 19; j++)
+				arr[i][j] = sc.nextInt();
+
+		// 모든 좌표에서 다 확인해봅니다.
+		for(int i = 0; i < 19; i++) {
+			// 격자를 벗어나지 않을 범위로만 잡습니다.
+			for(int j = 0; j < 19; j++) {
+				if(arr[i][j] == 0) continue;
+
+				for(int k = 0; k < DIR_NUM; k++) {
 					int curt = 1;
 					int curx = i;
-					int cury = j; // 현재 위치랑 탐색할 위치 분리
+					int cury = j;
 					while(true) {
 						int nx = curx + dx[k];
-						int ny = cury + dy[k]; // 탐색할 위치 계산
-						if(!inRange(nx, ny))
+						int ny = cury + dy[k];
+						if(inRange(nx, ny) == false)
 							break;
-						if(area[nx][ny] != area[i][j])
+						if(arr[nx][ny] != arr[i][j])
 							break;
-						// 계산된 탐색할 위치 검증
 						curt++;
 						curx = nx;
 						cury = ny;
-						// 해당 위치로 이동
 					}
 					if(curt == 5) {
-						System.out.println(area[i][j]);
+						System.out.println(arr[i][j]);
 						System.out.print((i + 2 * dx[k] + 1) + " " + (j + 2 * dy[k] + 1));
 						System.exit(0);
 					}
 				}
 			}
 		}
-        System.out.println(0);
-	}
-	private static boolean inRange(int x, int y){
-		if(x >= 19 || y >= 19){
-			return false;
-		}else return x >= 0 && y >= 0;
+		System.out.print(0);
 	}
 }
