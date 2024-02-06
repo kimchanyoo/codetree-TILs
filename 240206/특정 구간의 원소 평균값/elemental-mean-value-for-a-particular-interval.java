e2ea82b19692acd5a3f61d958ecc1db7faaea8ac
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-06 17:06:10 +0900

    Title: 특정 구간의 원소 평균값, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240206/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    static boolean isFloat(int sum, int length){
+		return sum % length == 0;
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] numList = new int[n];
+		int cnt = 0;
+
+		for(int i = 0; i < n; i++){
+			numList[i] = sc.nextInt();
+		}
+
+		for(int i = 0; i < n; i++){
+			for(int j = i; j < n; j++){
+				int sum = 0;
+				int length = j - i + 1;
+				for(int k = i; k <= j; k++){
+					sum += numList[k];
+				}
+				if(isFloat(sum, length)){
+					for(int k : numList){
+						if(sum / length == k){
+							cnt++;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		System.out.println(cnt);
+    }
+}
