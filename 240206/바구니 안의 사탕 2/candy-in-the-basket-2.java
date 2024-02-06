5db1ecce2f0389e9306c8791ce08b97387531b6c
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-06 18:13:06 +0900

    Title: 바구니 안의 사탕 2, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 240206/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -12,7 +12,7 @@ public class Main {
 			candyCount[sc.nextInt()] += num;
 		}
 		if(2 * k > 100){
-			for(int i = 0; i <= 100 - k; i++){
+			for(int i = 0; i <= 100; i++){
 				int sum = 0;
 				for(int j = i; j <= 100; j++){
 					sum += candyCount[j];
