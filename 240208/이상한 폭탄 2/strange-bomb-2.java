03241f49a65750ebfaae41ffa558432c1eb5d44f
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-08 23:25:02 +0900

    Title: 이상한 폭탄 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240208/이상한 폭탄 2/strange-bomb-2.java
+++ 240208/이상한 폭탄 2/strange-bomb-2.java
@@ -5,20 +5,30 @@ public class Main {
 		int n = sc.nextInt();
 		int k = sc.nextInt();
 		int[] bomb = new int[n];
-		int max = Integer.MIN_VALUE;
+		int max = - 1;
 
 		for(int i = 0; i < n; i++){
 			bomb[i] = sc.nextInt();
 		}
 
-		for(int i = 0; i < n - k; i++){
-			for(int j = i + 1; j <= i + k; j++){
-				if(bomb[i] == bomb[j]){
-					max = Math.max(max, bomb[i]);
+		if(n == k){
+			for(int i = 0; i < n - 1; i++){
+				for(int j = i + 1; j < n; j++){
+					if(bomb[i] == bomb[j]){
+						max = Math.max(max, bomb[i]);
+					}
+				}
+			}
+		}else{
+			for(int i = 0; i < n - k; i++){
+				for(int j = i + 1; j <= i + k; j++){
+					if(bomb[i] == bomb[j]){
+						max = Math.max(max, bomb[i]);
+					}
 				}
 			}
 		}
-
+		
 		System.out.println(max);
     }
 }
