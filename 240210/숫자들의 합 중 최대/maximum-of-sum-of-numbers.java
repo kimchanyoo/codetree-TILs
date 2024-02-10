f258f6be264eabc47cd906276752ca446189d8ef
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-10 22:29:09 +0900

    속도 개선 버전 추가

--- 240210/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
+++ 240210/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
@@ -26,3 +26,30 @@ public class Main {
         return sum;
     }
 }
+
+/*
+속도 개선 버전
+    import java.util.Scanner;
+
+public class Main {
+    public static int x, y;
+
+    public static int cal(int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt(); y = sc.nextInt();
+        int max_sum = 0;
+        for (int i = x; i <= y; i++) 
+            max_sum = Math.max(max_sum, cal(i));
+        System.out.print(max_sum);
+    }
+}
+*/
