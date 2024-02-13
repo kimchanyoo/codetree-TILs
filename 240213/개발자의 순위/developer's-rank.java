a41ccd0e98a967979265dfeb9e16f1f51e010530
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-13 19:41:10 +0900

    Title: 개발자의 순위, Time: 179ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/개발자의 순위/developer's-rank.java
+++ 240213/개발자의 순위/developer's-rank.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
+    static int k, n;                       // k: 경기의 수, n: 개발자 수
+    static int[][] play;                    // 경기 결과 순위
+    static int[][] winCnt;                  // winCnt[i][j]: i번 개발자가 j번 개발자보다 높은 순위
+    static int ans;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		int k = sc.nextInt();
-		int n = sc.nextInt();
-		int[][] rank = new int[k][n];
-		int[] cntDeveloper = new int[n];
-		int cnt = 0;
+        k = sc.nextInt();      // k번 경기에 대해 n명의 개발자 순위가 주어짐
+        n = sc.nextInt();
 
-		for(int i = 0; i < k; i++){
-			for(int j = 0; j < n; j++){
-				rank[i][j] = sc.nextInt();
-			}
-		}
+        play = new int[k][n];
+        winCnt = new int[n + 1][n + 1];
 
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < k; j++){
-				cntDeveloper[rank[j][i] - 1]++;
-			}
-			for(int j = 0; j < n; j++){
-				if(cntDeveloper[j] == k){
-					cnt += n - (i + 1);
-					cntDeveloper[j] = 0;
-				}
-			}
-		}
+        for (int i = 0; i < k; i++) {
+            // 입력
+            for (int j = 0; j < n; j++)
+                play[i][j] = sc.nextInt();
 
-		System.out.println(cnt);
+            // 이긴 횟수 저장
+            for (int j = 0; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    winCnt[play[i][j]][play[i][k]]++;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (winCnt[i][j] == k) ans++;  // 항상(k번의 경기수에 대해) i가 j보다 높은 순위라면
+            }
+        }
+
+        System.out.println(ans);
+        sc.close();
     }
 }
