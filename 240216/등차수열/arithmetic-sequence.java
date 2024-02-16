37f96bdb5d325d721720bec5161383fe85280326
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-16 22:03:09 +0900

    Title: 등차수열, Time: 185ms, Memory: 10MB, Status: Passed - CodeTree

--- 240216/등차수열/arithmetic-sequence.java
+++ 240216/등차수열/arithmetic-sequence.java
@@ -27,6 +27,23 @@ public class Main {
 					}
 				}
 			}
+
+            /*
+            이렇게도 풀 수 있음
+            // 각 숫자에 대해 
+            // 등차수열의 개수를 확인합니다.
+            for(int x = 1; x <= MAX_A; x++) {
+                // 모든 쌍을 만들어 등차수열의 개수를 확인합니다.
+                int cnt = 0;
+
+                for(int i = 0; i < n; i++)
+                    for(int j = i + 1; j < n; j++)
+                        if(arr[i] + arr[j] == 2 * x)
+                            cnt++;
+            
+                ans = Math.max(ans, cnt);
+            }
+            */
 			max = Math.max(max, count);
 		}
 
