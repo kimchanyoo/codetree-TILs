265ed57ec4a805fdd57b2d338ea2e51e4ef9b1c7
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-18 16:40:15 +0900

    Title: 3개의 선 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240218/3개의 선 2/three-lines-2.java
+++ 240218/3개의 선 2/three-lines-2.java
@@ -1,66 +1,84 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_X = 10;
+    public static final int MAX_N = 20;
+    
+    public static int n;
+    public static int[] x = new int[MAX_N];
+    public static int[] y = new int[MAX_N];
 
-    static int[] x;
-    static int[] y;
-    static int[][] xy;
-    static String[] s = {"xxx", "xxy", "xyx", "yxx", "yyy", "yyx", "yxy", "xyy"};
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
 
-        int n = sc.nextInt();
-
-        x = new int[n];
-        y = new int[n];
-        xy = new int[11][11];
         for(int i = 0; i < n; i++) {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
-
-            xy[x[i]][y[i]] = 1;
         }
 
-        for(int i = 0; i < s.length; i++) {
-            char c1 = s[i].charAt(0);
-            char c2 = s[i].charAt(1);
-            char c3 = s[i].charAt(2);
+        int ans = 0;
 
-            for(int j = 0; j <= 5; j++) {
-                for(int k = 0; k <= 5; k++) {
-                    for(int l = 0; l <= 5; l++) {
-                        if(j == k || k == l || j == l) continue;
-                        int sum = 0;
-
-                        sum += go(j, c1);
-                        sum += go(k, c2);
-                        sum += go(l, c3);
-
-                        if(sum == n) {
-                            System.out.println(1);
-                            System.exit(0);
-                        }
+        // 모든 직선에 대해 전부 시도해 봅니다.
+        for(int i = 0; i <= MAX_X; i++)
+            for(int j = 0; j <= MAX_X; j++)
+                for(int k = 0; k <= MAX_X; k++) {
+                    // success : 직선 3개로 모든 점을 지나게 할 수 있으면 true
+                    boolean success = true;
+                    // x축에 평행한 직선 3개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(x[l] == i || x[l] == j || x[l] == k)
+                            continue;
+                        
+                        success = false;
                     }
+                    if(success)
+                        ans = 1;
+
+                    // x축에 평행한 직선 2개와 y축에 평행한 직선 1개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    success = true;
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(x[l] == i || x[l] == j || y[l] == k)
+                            continue;
+                        
+                        success = false;
+                    }
+                    if(success)
+                        ans = 1;
+                    
+                    // x축에 평행한 직선 1개와 y축에 평행한 직선 2개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    success = true;
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(x[l] == i || y[l] == j || y[l] == k)
+                            continue;
+                        
+                        success = false;
+                    }
+                    if(success)
+                        ans = 1;
+                    
+                    // y축에 평행한 직선 3개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    success = true;
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(y[l] == i || y[l] == j || y[l] == k)
+                            continue;
+                        
+                        success = false;
+                    }
+                    if(success)
+                        ans = 1;
+                    
                 }
-            }
-        }
 
-        System.out.println(0);
-    }
-    static int go(int n, char ch) {
-        int sum = 0;
-        if(ch == 'x') {
-            for(int i = 0; i <= 10; i++) {
-                sum += xy[i][n];
-            }
-        } else {
-            for(int i = 0; i <= 10; i++) {
-                sum += xy[n][i];
-            }
-        }
-
-        return sum;
+        System.out.print(ans);
     }
 }
