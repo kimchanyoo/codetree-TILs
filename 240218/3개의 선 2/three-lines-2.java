74cb9525180215535e25d5cf2df1e378f3415a98
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-18 16:40:03 +0900

    Title: 3개의 선 2, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240218/3개의 선 2/three-lines-2.java
+++ 240218/3개의 선 2/three-lines-2.java
@@ -1,52 +1,66 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int MAX_X = 10;
-	static int MAX_N = 20;
-	static int[][] points = new int[MAX_N][2];
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		for (int i = 0; i < n; i++) {
-			points[i][0] = scanner.nextInt();
-			points[i][1] = scanner.nextInt();
-		}
 
+    static int[] x;
+    static int[] y;
+    static int[][] xy;
+    static String[] s = {"xxx", "xxy", "xyx", "yxx", "yyy", "yyx", "yxy", "xyy"};
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
 
-// 모든 직선에 대해 전부 시도해 봅니다.
-		for (int i = 0; i <= MAX_X; i++) {
-			for (int j = 0; j <= MAX_X; j++) {
-				for (int k = 0; k <= MAX_X; k++) {
-// 가능한 모든 조합을 확인해봅니다.
-					String[] combs = {"xxx", "xxy", "xyx", "yxx", "yyy"};
-					for (String comb : combs) {
-						if (isPossible(comb, new int[]{i, j, k})) {
-							System.out.println(1);
-							System.exit(0);
-						}
-					}
-				}
-			}
-		}
+        int n = sc.nextInt();
 
-		System.out.println(0);
-	}
-	private static boolean isPossible(String comb, int[] vals) {
-		for (int[] point : points) {
-			boolean passed = false;
-			for (int i = 0; i < comb.length(); i++) {
-				char ch = comb.charAt(i);
-				int val = vals[i];
-				int x = point[0];
-				int y = point[1];
-				if ((ch == 'x' && x == val) || (ch == 'y' && y == val)) {
-					passed = true;
-				}
-			}
-			if (!passed) {
-				return false;
-			}
-		}
-		return true;
-	}
+        x = new int[n];
+        y = new int[n];
+        xy = new int[11][11];
+        for(int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+
+            xy[x[i]][y[i]] = 1;
+        }
+
+        for(int i = 0; i < s.length; i++) {
+            char c1 = s[i].charAt(0);
+            char c2 = s[i].charAt(1);
+            char c3 = s[i].charAt(2);
+
+            for(int j = 0; j <= 5; j++) {
+                for(int k = 0; k <= 5; k++) {
+                    for(int l = 0; l <= 5; l++) {
+                        if(j == k || k == l || j == l) continue;
+                        int sum = 0;
+
+                        sum += go(j, c1);
+                        sum += go(k, c2);
+                        sum += go(l, c3);
+
+                        if(sum == n) {
+                            System.out.println(1);
+                            System.exit(0);
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(0);
+    }
+    static int go(int n, char ch) {
+        int sum = 0;
+        if(ch == 'x') {
+            for(int i = 0; i <= 10; i++) {
+                sum += xy[i][n];
+            }
+        } else {
+            for(int i = 0; i <= 10; i++) {
+                sum += xy[n][i];
+            }
+        }
+
+        return sum;
+    }
 }
