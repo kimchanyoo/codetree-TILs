7206eec8c714cb30bfb6fa6d81209f312f791e59
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-20 22:26:47 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 337ms, Memory: 29MB, Status: Passed - CodeTree

--- 240220/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
+++ 240220/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		String str = sc.next();
 		int count = 1;
@@ -11,17 +11,28 @@ public class Main {
 		while(true){
 			boolean isPresent = false;
 			for(int i = 0; i <= strLength - count; i++){
-				String temp = str;
-				String[] tempList = temp.split("");
-				StringBuilder replace = new StringBuilder();
+				String[] tempList = str.split("");
+				String replace1 = "";
+				String replace2 = "";
+				int tempCount = 0;
+
 				for(int j = i; j < i + count; j++){
-					replace.append(tempList[j]);
+					replace1 += tempList[j];
 				}
-				temp = temp.replaceAll(replace.toString(), "");
-				if(temp.length() == strLength - (count * 2)){
-					count++;
-					isPresent = true;
-					break;
+
+				for(int j = 0; j <= strLength - count; j++){
+					replace2 = "";
+					for(int k = j; k < j + count; k++){
+						replace2 += tempList[k];
+					}
+					if(replace1.equals(replace2)){
+						tempCount++;
+					}
+					if(tempCount == 2){
+						count++;
+						isPresent = true;
+						break;
+					}
 				}
 			}
 			if(!isPresent){
@@ -30,5 +41,5 @@ public class Main {
 		}
 
 		System.out.println(count);
-    }
+	}
 }
