83f37d1b7a6c26b7c565d00263baee7f583cbf73
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-21 22:18:22 +0900

    Title: Date to Date, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240221/Date to Date/date-to-date.java
+++ 240221/Date to Date/date-to-date.java
@@ -8,11 +8,18 @@ public class Main {
 		int m2 = sc.nextInt();
 		int d2 = sc.nextInt();
 		int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 30, 30, 31, 30, 31};
-		int count = (monthDays[m1 - 1] - d1) + d2 + 1;
+		int count;
 
-		if(m1 + 1 != m2){
-			for(int i = m1 + 1; i < m2; i++){
-				count += monthDays[i - 1];
+		if(m1 == m2 && d1 == d2){
+			count = 0;
+		}else if(m1 == m2){
+			count = d2 - d1 + 1;
+		}else{
+			count = (monthDays[m1 - 1] - d1) + d2 + 1;
+			if(m1 + 1 != m2){
+				for(int i = m1 + 1; i < m2; i++){
+					count += monthDays[i - 1];
+				}
 			}
 		}
 
