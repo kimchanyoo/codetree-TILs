6bad554651f876090e13f7f70ac0832b67a96393
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-21 22:38:24 +0900

    Title: Date to Date, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240221/Date to Date/date-to-date.java
+++ 240221/Date to Date/date-to-date.java
@@ -2,27 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
 		int m1 = sc.nextInt();
 		int d1 = sc.nextInt();
 		int m2 = sc.nextInt();
 		int d2 = sc.nextInt();
-		int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 30, 30, 31, 30, 31};
-		int count;
+		int[] monthDays = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int elapsedDays = 1;
 
-		if(m1 == m2 && d1 == d2){
-			count = 1;
-		}else if(m1 == m2){
-			count = d2 - d1 + 1;
-		}else{
-			count = (monthDays[m1 - 1] - d1) + d2 + 1;
-			if(m1 + 1 != m2){
-				for(int i = m1 + 1; i < m2; i++){
-					count += monthDays[i - 1];
-				}
-			}
+		for(int i = m1; i < m2; i++){
+			elapsedDays += monthDays[i];
 		}
 
-		System.out.println(count);
+		elapsedDays -= d1;
+		elapsedDays += d2;
+
+		System.out.println(elapsedDays);
     }
 }
