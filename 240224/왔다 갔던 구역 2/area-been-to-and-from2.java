e56f44ccd06fc5bfb3faf6da63c01c21e32e219e
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-24 17:06:26 +0900

    Title: 왔다 갔던 구역 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240224/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] x = new int[n];
+		String[] direction = new String[n];
+		int[] line = new int[2001];
+		int location = 1000;
+		int count = 0;
+
+		for(int i = 0; i < n; i++){
+			x[i] = sc.nextInt();
+			direction[i] = sc.next();
+		}
+
+		for(int i = 0; i < n; i++){
+			if(direction[i].equals("L")){
+				for(int j = location; j >= location - x[i]; j--){
+					line[j]++;
+				}
+				location = location - x[i];
+			}else if(direction[i].equals("R")){
+				for(int j = location; j <= location + x[i]; j++){
+					line[j]++;
+				}
+				location = location + x[i];
+			}
+		}
+
+		boolean isPresent = false;
+		for(int i = 0; i < line.length; i++){
+			if(line[i] >= 2 && !isPresent){
+				isPresent = true;
+				count--;
+			}else if(line[i] < 2){
+				isPresent = false;
+			}
+
+			if(isPresent){
+				count++;
+			}
+		}
+
+		System.out.println(count);
+    }
+}
