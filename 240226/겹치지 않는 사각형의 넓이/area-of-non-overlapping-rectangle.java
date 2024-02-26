ff57c75cb1e9834d4b30cb6ee2456fe10af99479
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-26 13:33:47 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240226/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int[][] area = new int[2001][2001];
+		int count = 0;
+
+		for(int i = 0; i < 3; i++){
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
+			if(i == 2){
+				for(int j = y1; j < y2; j++){
+					for(int k = x1; k < x2; k++){
+						area[j][k] += 2;
+					}
+				}
+			}else{
+				for(int j = y1; j < y2; j++){
+					for(int k = x1; k < x2; k++){
+						area[j][k]++;
+					}
+				}
+			}
+
+		}
+
+		for(int i = 0; i < area.length; i++){
+			for(int j = 0; j < area[i].length; j++){
+				if(area[i][j] == 1){
+					count++;
+				}
+			}
+		}
+
+		System.out.println(count);
+    }
+}
