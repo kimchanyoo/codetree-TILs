e1e1d103a6162f8e50094691303cf19720f36ef7
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-28 21:26:58 +0900

    Title: 연속되는 수 3, Time: 211ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240228/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] numList = new int[n];
+		int max = -1;
+		int count = 0;
+
+		for(int i = 0; i < n; i++){
+			numList[i] = sc.nextInt();
+		}
+
+		for(int i = 0; i < n; i++){
+			if((i >= 1) && ((numList[i] > 0 && numList[i - 1] > 0)
+					|| (numList[i] < 0 && numList[i - 1] < 0))){
+				count++;
+			}else{
+				count = 1;
+			}
+			max = Math.max(max, count);
+		}
+
+		System.out.println(max);
+    }
+}
