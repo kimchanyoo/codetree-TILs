d2c29944dcf4f5fb416299fe20fffee272aa36a2
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-28 21:33:49 +0900

    Title: 연속되는 수 4, Time: 184ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240228/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] numList = new int[n];
+		int max = -1;
+		int count = 0;
+
+		for(int i = 0; i < n; i++){
+			numList[i] = sc.nextInt();
+		}
+
+		for(int i = 0; i < n; i++){
+			if(i >= 1 && numList[i] > numList[i - 1]){
+				count++;
+			}else{
+				count = 1;
+			}
+			max = Math.max(max, count);
+		}
+
+		System.out.println(max);
+    }
+}
