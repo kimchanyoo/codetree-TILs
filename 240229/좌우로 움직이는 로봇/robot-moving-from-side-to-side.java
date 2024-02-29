f61f8831133ea40f17ffc9f49967a255f854a930
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-29 20:14:34 +0900

    Title: 좌우로 움직이는 로봇, Time: 1182ms, Memory: 58MB, Status: Passed - CodeTree

--- 240229/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240229/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
 	static final int OFFSET = 1000000;
+	static int move = 0;
 	public static void main(String[] args){
 		int n = sc.nextInt();
 		int m = sc.nextInt();
@@ -13,7 +14,7 @@ public class Main {
 		caculation(n, a);
 		caculation(m, b);
 
-		for(int i = 1; i < OFFSET + 1; i++){
+		for(int i = 1; i <= move; i++){
 			if(a[i] == b[i]){
 				if(a[i - 1] != b[i - 1]){
 					count++;
@@ -25,22 +26,30 @@ public class Main {
 	}
 
 	private static void caculation(int num, int[] arr) {
-		int location = 1;
+		int location = 0;
 
 		for(int i = 0; i < num; i++){
 			int t = sc.nextInt();
 			char direction = sc.next().charAt(0);
 			if(direction == 'L'){
 				for(int j = 0; j < t; j++){
-					arr[location] = arr[location - 1] - 1;
+					arr[location + 1] = arr[location] - 1;
 					location++;
 				}
 			}else{
 				for(int j = 0; j < t; j++){
-					arr[location] = arr[location - 1] + 1;
+					arr[location + 1] = arr[location] + 1;
 					location++;
 				}
 			}
+		}
+
+		for(int i = location; i < OFFSET; i++){
+			arr[i + 1] = arr[i];
+		}
+
+		if(location >= move){
+			move = location;
 		}
 	}
 }
