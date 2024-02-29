25053a45e7c845b1c51f9f3a175bde2b834d24ba
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-02-29 19:11:18 +0900

    Title: 선두를 지켜라, Time: 320ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240229/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+	static final int MAX_T = 1000000;
+	static int[] a = new int[MAX_T + 1];
+	static int[] b = new int[MAX_T + 1];
+	public static void main(String[] args){
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		caculation(n, a);
+		caculation(m, b);
+		int first = 0;
+		int count = 0;
+
+		for(int i = 1; i < MAX_T + 1; i++){
+			if(i == 1){
+				if(a[i] > b[i]){
+					first = 1;
+				}else if(a[i] < b[i]){
+					first = 2;
+				}
+			}else{
+				if(a[i] > b[i] && first == 2){
+					first = 1;
+					count++;
+				}else if(a[i] < b[i] && first == 1){
+					first = 2;
+					count++;
+				}else if(first == 0){
+					if(a[i] > b[i]){
+						first = 1;
+						count++;
+					}else if(a[i] < b[i]){
+						first = 2;
+						count++;
+					}
+				}
+			}
+		}
+
+		System.out.println(count);
+	}
+
+	private static void caculation(int num, int[] arr) {
+		int location = 1;
+		int temp = 0;
+
+		for(int i = 0; i < num; i++){
+			int v = sc.nextInt();
+			int t = sc.nextInt();
+			for(int j = 0; j < t; j++){
+				arr[location] = temp + v;
+				temp = arr[location];
+				location++;
+			}
+		}
+	}
+}
