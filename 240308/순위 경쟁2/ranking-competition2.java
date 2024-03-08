b8a4fe0e3632d9638a91db6134cbe439144cc85b
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-03-08 22:04:32 +0900

    Title: 순위 경쟁2, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240308/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a = 0;
+		int b = 0;
+		String rank = "";
+		int count = 0;
+
+		for(int i = 0; i < n; i++){
+			char c = sc.next().charAt(0);
+			int s = sc.nextInt();
+			if(c == 'A'){
+				a += s;
+			}else if(c == 'B'){
+				b += s;
+			}
+			if(a > b && !rank.equals("A")){
+				rank = "A";
+				count++;
+			}else if(a < b && !rank.equals("B")){
+				rank = "B";
+				count++;
+			}else if(a == b && !rank.equals("AB")){
+				rank = "AB";
+				count++;
+			}
+		}
+
+		System.out.println(count);
+    }
+}
