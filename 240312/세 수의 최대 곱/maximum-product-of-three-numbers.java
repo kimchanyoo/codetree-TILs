caf5830b6be8abb1b931720df7b6e90b050dd7a9
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-03-12 17:23:34 +0900

    Title: 세 수의 최대 곱, Time: 835ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240312/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for(int i = 0; i < n; i++){
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+
+		System.out.println(Math.max((arr[0] * arr[1] * arr[n-1]), (arr[n - 3] * arr[n - 2] * arr[n - 1])));
+    }
+}
