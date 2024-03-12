34f88538e12023d19ecccd8e7b29446e6dbc9b77
Chanyoo Kim <57432440+kimchanyoo@users.noreply.github.com>
2024-03-12 16:42:35 +0900

    Title: L, R 그리고 B, Time: 120ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240312/L, R 그리고 B/l-r-and-b.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		char[][] arr = new char[10][10];
+		int bHeight = 0;
+		int lHeight = 0;
+		int rHeight = 0;
+		int bWidth = 0;
+		int lWidth = 0;
+		int rWidth = 0;
+
+		for(int i = 0; i < 10; i++){
+			arr[i] = sc.next().toCharArray();
+		}
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				if(arr[i][j] == 'B'){
+					bHeight = i + 1;
+					bWidth = j + 1;
+				}else if(arr[i][j] == 'L'){
+					lHeight = i + 1;
+					lWidth = j+ 1;
+				}else if(arr[i][j] == 'R'){
+					rHeight = i + 1;
+					rWidth = j + 1;
+				}
+			}
+		}
+
+		System.out.println(Math.abs(bHeight - lHeight) + Math.abs(bWidth - lWidth) - 1);
+    }
+}
